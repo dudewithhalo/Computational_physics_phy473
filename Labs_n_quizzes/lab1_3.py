bfc209d0ea5eb83d845f25b77dbc6312f5871db9
dudewithhalo <pankajks@iitk.ac.in>
2021-08-29 21:54:13 +0530

    Create lab1_3.py

---
+++ Labs_n_quizzes/lab1_3.py
@@ -0,0 +1,14 @@
+'''
+Write a python program that takes an integer as input and
+converts it into a binary number. You should print the binary number as your output.
+
+Example:
+INPUT:
+123
+
+OUTPUT:
+0b1111011
+'''
+
+x = int(input('Enter the number: '))
+print(bin(x))
